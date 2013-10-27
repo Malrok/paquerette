8a805cc06279ebff89d86e4df3b82679c148219a
Malrok <themalrok@gmail.com>
2013-10-27 15:33:13 +0100

    Alpha on textures in now taken in account when drawing

--- MRKOGL/src/com/mrk/oglext/renderers/GLRenderer.java
+++ MRKOGL/src/com/mrk/oglext/renderers/GLRenderer.java
@@ -94,6 +94,10 @@ public class GLRenderer implements GLSurfaceView.Renderer {
 		gl.glTranslatef(0, 0, -4); 
 		// Draw our scene.
 
+		// Enables alpha on textures
+		gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+		gl.glEnable(GL10.GL_BLEND);
+		
 //		topLeft = GetWorldCoords(new float[]{0,0});
 //		bottomRight = GetWorldCoords(new float[]{screenW ,screenH});
 		
